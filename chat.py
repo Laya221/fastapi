83499b4d955ad7d6c8b34ae551d10862437e7250
Laya221 <30593832+Laya221@users.noreply.github.com>
2023-02-10 19:26:25 +0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -2,25 +2,25 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import uvicorn
-from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
-from chatterbot.trainers import ChatterBotCorpusTrainer
+#from chatterbot import ChatBot
+#from chatterbot.trainers import ListTrainer
+#from chatterbot.trainers import ChatterBotCorpusTrainer
 import time
-import spacy.cli 
-spacy.cli.download("en_core_web_md")
+#import spacy.cli 
+#spacy.cli.download("en_core_web_md")
 time.clock = time.time
 app = FastAPI()
 
 templates = Jinja2Templates(directory="")
 
 
-chatbot = ChatBot("Chatpot")
+#chatbot = ChatBot("Chatpot")
 #chatbot.storage.drop()
-trainer = ListTrainer(chatbot)
+#trainer = ListTrainer(chatbot)
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/getChatBotResponse")
 def get_bot_response(msg: str):
-    return str(chatbot.get_response(msg))
+    return str(msg)#str(chatbot.get_response(msg))
