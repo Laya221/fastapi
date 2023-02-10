9ddec09c3e6ff71734412ca8c3b6e85e883ed6ab
Laya221 <30593832+Laya221@users.noreply.github.com>
2023-02-10 18:28:51 +0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,24 +1,24 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import uvicorn
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
+import time
+time.clock = time.time
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
 
-@app.post("/chat")
-def chat(message: str):
-    response = "I'm sorry, I don't understand your message."
-    if "hello" in message.lower() or "hi" in message.lower():
-        response = "Hello! How can I help you with web development today?"
-    elif "html" in message.lower():
-        response = "HTML stands for Hypertext Markup Language and is used for creating web pages."
-    elif "css" in message.lower():
-        response = "CSS stands for Cascading Style Sheets and is used for styling web pages."
-    elif "javascript" in message.lower():
-        response = "JavaScript is a programming language used for creating interactive elements on web pages."
-    return {"message": response}
+chatbot = ChatBot("Chatpot")
+#chatbot.storage.drop()
+trainer = ListTrainer(chatbot)
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/getChatBotResponse")
+def get_bot_response(msg: str):
+    return str(chatbot.get_response(msg))
