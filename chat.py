474fedde498f552ce1ae1adbf5afc7432125e1b4
Laya221 <30593832+Laya221@users.noreply.github.com>
2023-02-11 17:00:43 +0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -2,32 +2,48 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import uvicorn
-from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
-from chatterbot.trainers import ChatterBotCorpusTrainer
+import json 
+import numpy as np
+from tensorflow import keras
+from sklearn.preprocessing import LabelEncoder
+import random
+import pickle
 import time
 time.clock = time.time
-import spacy
 
-nlp = spacy.load("en_core_web_sm")
-
-if not nlp.valid:
-    import spacy.cli
-    spacy.cli.download("en_core_web_sm")
-
+with open("intents.json") as file:
+    data = json.load(file)
 app = FastAPI()
 
 templates = Jinja2Templates(directory="")
 
+def chat(inp):
+    # load trained model
+    model = keras.models.load_model('chat_model')
 
-# Create a ChatBot instance
-chatbot = ChatBot(name='MyChatBot', read_only=True, logic_adapters=['chatterbot.logic.BestMatch'])
+    # load tokenizer object
+    with open('tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
 
-# Create a ChatterBot trainer
-trainer = ChatterBotCorpusTrainer(chatbot)
+    # load label encoder object
+    with open('label_encoder.pickle', 'rb') as enc:
+        lbl_encoder = pickle.load(enc)
+    
+     # parameters
+    max_len = 20
+    
+   
+        
 
-# Train the chatbot using the English corpus
-trainer.train("chatterbot.corpus.english")
+    result = model.predict(keras.preprocessing.sequence.pad_sequences(tokenizer.texts_to_sequences([inp]),
+                                             truncating='post', maxlen=max_len))
+    tag = lbl_encoder.inverse_transform([np.argmax(result)])
+
+    for i in data['intents']:
+        if i['tag'] == tag:
+            print("ChatBot:" , np.random.choice(i['responses']))
+
+
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
@@ -35,4 +51,4 @@ def home(request: Request):
 
 @app.get("/getChatBotResponse")
 def get_bot_response(msg: str):
-    return str(chatbot.get_response(msg))
+    return str(chat(msg))
