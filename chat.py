6c730ddecc48c1f7ec5a9d5a1c12274bb20f8889
Laya221 <30593832+Laya221@users.noreply.github.com>
2023-03-30 20:57:40 +0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,134 +1,66 @@
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import uvicorn
+from fastapi import APIRouter, Request, Depends, responses, status
+from fastapi.templating import Jinja2Templates
 
+import numpy as np
+from langchain.chains.conversation.memory import ConversationBufferMemory
+from langchain import OpenAI, LLMChain, PromptTemplate
+from langchain.chat_models import ChatOpenAI
+import random
 import time
 time.clock = time.time
 import openai
+import os 
 
 temp='%s%k%-%N%V%b%i%n%T%V%Y%L%a%W%N%T%M%9%I%o%u%x%z%T%3%B%l%b%k%F%J%y%h%0%n%P%X%A%s%J%h%7%8%t%W%h%a%2%f%d%z'
 api_key=""
 for i in range(1,len(temp),2):
     api_key+=temp[i]
 
-openai.api_key = api_key
-COMPLETIONS_MODEL = "text-davinci-002"
+os.environ["OPENAI_API_KEY"] = api_key
 
 app = FastAPI()
 templates = Jinja2Templates(directory="")
-class static:
-   first_login=True
-   user_info={}
-   step='step1'
-   history=[]
-   my_set=set()
-   my_set1=set()
-   my_set2=set()
-def A2ZBot(prompt):
-  bot_response=openai.Completion.create(
-        prompt=prompt,
-        temperature=0.9,
-        max_tokens=300,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-        model=COMPLETIONS_MODEL
-    )["choices"][0]["text"].strip(" \n")
-  return bot_response
-def check(bot_response,user_response,problem):
-  prompt="""is User response verify the following condition "{}" in following conversation ? return 'yes' if it is true else return 'no' " .\n Bot: {} \nUser: {}""".format(problem,bot_response.strip(),user_response.strip())
-  temp=A2ZBot(prompt)
-  print(prompt)
-  print(temp)
-  if "no".lower() in temp.lower():
-    prompt="""give user example  response for this 'Bot:{}'  """.format(bot_response)
-    print('Correct: ',prompt)
-    result=A2ZBot(prompt)
-    return result
-  else:
-    return False
 
-def conversation(user_response):
-  if static.step=='step1':
-        static.step='step2'
-        bot_response= "What is your name?"
-        static.history.append(bot_response)
-        return bot_response
-  if static.step=='step2':
-    bot_response=check(static.history[-1],user_response,'user must to write his name')
-    if bot_response:
-      return 'This is Example for Good Response:\n'+bot_response
-    else:
-      static.history.append(user_response)
-      static.step='step3'
-      bot_response= "Nice to meet you.\nHow old are you?"
-      static.history.append(bot_response)
-      return bot_response
-  if static.step=='step3':
-    bot_response=check(static.history[-1],user_response,'User must to write his age ')
-    if bot_response:
-      
-      return 'This is Example for Good Response:\n'+bot_response
-    else:
-      static.history.append(user_response)
-      static.step='step4'
-      bot_response="""select your English Level:
-       A1
-       A2
-       B1
-       B2
-       C1
-       C2?"""
-      static.history.append(bot_response)
-      return bot_response
-  if static.step=='step4':
-    bot_response=check(static.history[-1],user_response,'User must to write his English Level from Bot options ')
-    if bot_response:
-      
-      return 'This is Example for Good Response:\n'+bot_response
-    else:
-      static.history.append(user_response)
-      static.step='step5'
-      bot_response="""Please choose one or two paths from the following pathes: Travel,Business,Fun/communication,Education,Default / General English """
-      static.history.append(bot_response)
-      return bot_response
-  if static.step=='step5':
-    bot_response=check(static.history[-1],user_response,'User must to write his English Path from Bot options')
-    if bot_response:
-      return 'This is Example for Good Response:\n'+bot_response
-    else:
-      static.history.append(user_response)
-      static.step='step6'
-      bot_response="""What are your interests?for example:Sport,Art,History,Technology,Gaming,Movies,...."""
-      static.history.append(bot_response)
-      return bot_response
-  if static.step=='step6':
-    bot_response=check(static.history[-1],user_response,'User must to write his his interests')
-    if bot_response:
-      return 'This is Example for Good Response:\n'+bot_response
-    else:
-      static.history.append(user_response)
-      static.step='step7'
-      code_=A2ZBot('Write python code to create dict called "user_info" with following keys "name,age,level,path,interests" and store user data from following history:\n {}'.format(static.history))
-      exec('static.'+code_)
-      return "Great !!!"
-  if static.step=='step7':
-    code=A2ZBot("""write python code to create set called my_set2 contain 20 vocabularies (no name) related to user interests '{}' """.format(static.user_info['path']))
-    exec('static.'+code)
-    code=A2ZBot("""write python code to create set called my_set1 contain 20 vocabularies (no name) related to user interests '{}' """.format(static.user_info['interests']))
-    exec('static.'+code)
-    static.step='step8'
+class static:
+   template="""You are A2ZBot assistant (Artificial Intelligence Bot) to learn user english,you can learn user  english vocabularies with examples and in a fun way.
+    Be kind,be creative , smart,interesting,friendly and funny.
+    don't ask questions back! 
+    use short questions.
+    Use Emojis.
+    don't generate chat_history.
+    allow to user to leave if he want and say goodbye.
+    you must do the folloing steps each seperatly:
+    step1:tell user about A2ZBot then ask user how is doing today.
+    step2:you must tell user some jokes or advices Especially if it's in a bad mood or doesn't want to learn.
+    step3:you must to ask user anout his feedback about A2Zbot "if the bot is helpful".
+    step4:finally you must to ask user if he is ready to start english journey for today after you did previous steps.
+    step5:create mini study plan for learning vocabulary based on english level and topics and goal.
+    Answer: return only A2ZBot response.
+    history:
+    {chat_history}
+    user: {question}
+    A2Zbot: generate one response.generate short question.don't generate chat_history.
+    """
+   memory=ConversationBufferMemory(memory_key="chat_history")
+def conversation(msg):
     
-  if static.step=='step8':
-    static.my_set=static.my_set1. union(static.my_set2)
-    prompt="""say 'Hello {}! ,You will learn following vocabularies {}' """.format(static.user_info['name'],static.my_set)
-    bot_response=A2ZBot(prompt)
-    static.step='step1'
-    static.history=[]  
-    return bot_response
-  
-      
+   prompt_template = PromptTemplate(input_variables=["chat_history","question"], template=static.template)
+   llm_chain = LLMChain(
+        llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.6,
+        max_tokens=300, n=1),
+        prompt=prompt_template,
+        verbose=False,
+        memory=static.memory,
+   
+        )
+   result=llm_chain.run(question=msg)
+   time.sleep(0.5)
+   return result.replace('A2ZBot:','',-1)
 
 
 
